19db49e09c595a2abd47edb7572bbc0eb40dc7e2
sameersami07 <shaikmsami@gmail.com>
2026-07-15 15:57:57 +0530

    chore: remove vercel.json configuration file

--- .vercel/output/functions/api/index.js.func/Server/models/CmsSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CmsSchema = new mongoose.Schema({
-  key: { type: String, required: true, unique: true },
-  value: { type: mongoose.Schema.Types.Mixed, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('CMS', CmsSchema);
--- .vercel/output/functions/api/index.js.func/api/index.js
+++
@@ -1,2 +0,0 @@
-const app = require('../Server/server.js');
-module.exports = app;
